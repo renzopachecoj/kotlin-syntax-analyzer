54d7171c49bce443a583c06dfd3b0ad805319232
Renzo Pacheco <renzopachecoj@gmail.com>
2020-07-07 11:09:57 -0500

    archivo base de practica

---
+++ analizador_lexico.py
@@ -0,0 +1,59 @@
+import ply.lex as lex
+
+reserved = {
+    'if' : 'IF',
+    'elif' : 'ELIF',
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'for' : 'FOR',
+    'echo' : 'ECHO'
+}
+
+tokens = ["MENOS", "MAS", "PRODUCTO", "DIVISION",
+          "NUMEROS", "LPAREN", "RPAREN", "IGUAL", "POTENCIA",
+          "ID", "COMPARA", "PUNTOS"] + list(reserved.values())
+
+t_MENOS = r'-'
+t_MAS = r'\+'
+t_PRODUCTO = r'\*'
+t_DIVISION = r'/'
+t_NUMEROS = r'[0-9]+'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_IGUAL = r'='
+t_POTENCIA = r'\\'
+t_COMPARA = r'[<>!=]='
+t_PUNTOS = r':'
+t_IF = r'if'
+t_ECHO = r'echo'
+t_WHILE = r'while'
+t_FOR = r'for'
+t_ELIF = r'elif'
+t_ignore = r'   '
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    return t
+
+def t_error(t):
+    print("No se ha reconocido '%s'"%t.value[0])
+    t.lexer.skip(1)
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+cadena = "for while"
+
+analizadorL = lex.lex()
+analizadorL.input(cadena)
+
+while True:
+    tokenRec = analizadorL.token()
+    if tokenRec!=None:
+        print(tokenRec)
+    else:
+        break
+
+lex.lex()
